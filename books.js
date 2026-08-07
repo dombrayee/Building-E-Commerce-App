b0f3e127d20fc16ccaf1681468be0671ddc66006
Chimezie Ndukwe <domcndukwe@gmail.com>
2026-08-07 09:32:24 -0400

    most recent version

--- books.js
+++ books.js
@@ -1,5 +1,41 @@
+function renderBooks() {
+const booksWrapper = document.querySelector(".books");
+
+const books = getBooks();
+
+ const bookHTML = books.map((book) => {
+return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span class="book__price--normal">$${book.originalPrice}</span> $${books[0].salePrice} 
+    </div>
+  </div>` 
+})
+.join("");
+
+console.log(bookHTML);
+
+booksWrapper.innerHTML = bookHTML;
+//booksWrapper.innerHTML = ;
+}
 
 
+setTimeout(() => {
+  renderBooks();
+});
+renderBooks();
 // FAKE DATA
 function getBooks() {
   return [
